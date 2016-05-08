6955f2e0c78dcf9a8ab1548b2b6ca8bedcac94e1
jbrown400 <justin.brown400@gmail.com>
2016-05-08 12:07:10 -0500

    Added current weather conditions under the quote section. Have one case in icon switch case set up. Will add the others next. Have current temp working.

--- page/static/page/script.js
+++ page/static/page/script.js
@@ -17,3 +17,43 @@ function checkTime(i) {
     if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
     return i;
 }
+
+update = function() {
+    var current = [];
+    var forecast = [];
+    var activity = [];
+    var monthNames = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]
+    g = new Date();
+    gTime = monthNames[g.getMonth()] + "-" + g.getDate();
+    //Gets and sets current conditions
+    $.ajax({
+        url: "http://api.wunderground.com/api/" + WEATHER_KEY + "/conditions/q/MO/Rolla.json",
+        dataType: 'json',
+        timeout: 5000,
+        success: function(data, status) {
+            $.each(data, function(i, item) {
+                current = item;
+            });
+            $("#temp_f").html(Math.round(current.temp_f));
+            
+            iconDisplay("#icon", String(current.weather))
+        },
+        error: function() {
+            $("#temp_f").html('Error');
+
+        }
+    });
+
+}
+
+
+function iconDisplay(icon, item) {
+    dir_name = '/static/page/bower_components/animated-climacons/climacons/svg-css/';
+    switch(item) {
+        case "Clear":
+            $(icon).attr('src', dir_name + 'sunFill.svg');
+              break;
+        default:
+            $(icon).html('Error/Other');
+    }
+}
